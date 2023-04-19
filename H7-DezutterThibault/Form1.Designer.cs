eb88612c0958dc3ef0d1c4e9ddf84f336e58edd4
Thibault Dezutter <thibault.dezutter@student.ppsjo.be>
2023-04-19 11:47:06 +0200

    kleur veranderd

--- H7-DezutterThibault/Form1.Designer.cs
+++ H7-DezutterThibault/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Location = new System.Drawing.Point(-1, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 70);
             this.button1.TabIndex = 0;
             this.button1.Text = "Dezutter Thibault";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
